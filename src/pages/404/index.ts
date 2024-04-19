53e7b4b527000abfe06000ab0d03acb03e8d5208
DShulga92@gmail.com <Dshulga92@gmail.com>
2024-04-19 21:32:41 +0400

    test

--- src/pages/404/index.ts
+++
@@ -1,9 +0,0 @@
-import Block from '../../utils/Block'
-import template from './error.hbs'
-import './error.scss'
-
-export default class Page404 extends Block {
-  render() {
-    return this.compile(template, this.props)
-  }
-}
--- src/pages/500/index.ts
+++
@@ -1,9 +0,0 @@
-import Block from '../../utils/Block'
-import template from './error.hbs'
-import './error.scss'
-
-export default class Page500 extends Block {
-  render() {
-    return this.compile(template, this.props)
-  }
-}
